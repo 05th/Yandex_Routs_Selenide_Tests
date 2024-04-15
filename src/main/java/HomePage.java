91e1562aa9476d4a740b5d9b226c9dd1be38a5c2
05 <qatsby@yandex.com>
2024-04-14 22:53:29 -0400

    locators added

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,4 +1,11 @@
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.*;
+
 public class HomePage {
+    private SelenideElement timeToStartHours = $("#form-input-hour");
+    private SelenideElement timeToStartMinute = $("#form-input-minute");
+    private SelenideElement inputFromField = $("#form-input-from");
+    private SelenideElement inputToField = $("#form-input-to");
 
 
 }
