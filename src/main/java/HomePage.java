8fc21c0da96fe380bbf38ceff8a67ed4d090ae42
05 <qatsby@yandex.com>
2024-04-15 21:55:58 -0400

    .

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,11 +1,44 @@
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomePage {
-    private SelenideElement timeToStartHours = $("#form-input-hour");
-    private SelenideElement timeToStartMinute = $("#form-input-minute");
+    private SelenideElement setHours = $("#form-input-hour");
+    private SelenideElement setMinutes = $("#form-input-minute");
     private SelenideElement inputFromField = $("#form-input-from");
     private SelenideElement inputToField = $("#form-input-to");
+    private SelenideElement typeOptimal = $("#form-mode-optimal");
+    private SelenideElement typeFastest = $("#form-mode-fastest");
+    private SelenideElement typeCustom = $("#form-mode-custom");
 
+
+    public void inputHours(String hours) {
+        setHours.setValue(hours);
+    }
+
+    public void inputMinutes(String minutes) {
+        setMinutes.setValue(minutes);
+    }
+
+    public void inputDataFrom(String from) {
+        inputFromField.setValue(from);
+    }
+
+    public void inputDataTo(String to) {
+        inputToField.setValue(to);
+    }
+
+    public void choiceTypeOptional() {
+        typeOptimal.click();
+    }
+
+    public void choiceTypeFastest() {
+        typeFastest.click();
+    }
+
+    public void choiceTypeCustom() {
+        typeCustom.click();
+    }
 
 }
--- src/test/java/HomePageTests.java
+++ src/test/java/HomePageTests.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class HomePageTests {
+    private static HomePage homePage;
+
+    @BeforeAll
+    public static void setUp() {
+        open("https://qa-routes.praktikum-services.ru/");
+        homePage = new HomePage();
+    }
+    @Test
+    public void testRoute() {
+        String from = "Усачева, 3";
+        String to = "Фрунзенская набережная, 46";
+        String hours = "10";
+        String minutes = "20";
+
+        homePage.inputHours(hours);
+        homePage.inputMinutes(minutes);
+        homePage.inputDataFrom(from);
+        homePage.inputDataTo(to);
+        homePage.choiceTypeOptional();
+
+    }
 }
