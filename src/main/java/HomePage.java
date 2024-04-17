86fffea06bb71d60bc76613bfecf01d288c17c7d
05 <qatsby@yandex.com>
2024-04-16 21:32:47 -0400

    locators added

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomePage {
+
+//    Css Locators (id)
     private SelenideElement setHours = $("#form-input-hour");
     private SelenideElement setMinutes = $("#form-input-minute");
     private SelenideElement inputFromField = $("#form-input-from");
@@ -11,6 +13,15 @@ public class HomePage {
     private SelenideElement typeOptimal = $("#form-mode-optimal");
     private SelenideElement typeFastest = $("#form-mode-fastest");
     private SelenideElement typeCustom = $("#form-mode-custom");
+
+    private SelenideElement typeCar = $("#from-type-car");
+    private SelenideElement typeWalk = $("from-type-walk");
+    private SelenideElement typeTaxi = $("from-type-taxi");
+    private SelenideElement typeBike = $("from-type-bike");
+    private SelenideElement typeScooter = $("from-type-scooter");
+    private SelenideElement typeDrive = $("from-type-drive");
+
+
 
 
     public void inputHours(String hours) {
